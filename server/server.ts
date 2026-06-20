fd6bf612591d5d18710c74811452abc1b9f8649b
lokeshgupta1347 <guptalokesh105@gmail.com>
2026-06-21 00:33:37 +0530

    backend server completed

--- server/server.ts
+++ server/server.ts
@@ -6,6 +6,8 @@ import authRouter from "./routes/authRoutes.js";
 import socialAuthRouter from "./routes/socialAuthRoutes.js";
 import accountRouter from "./routes/accountRoutes.js";
 import postRouter from "./routes/postRoutes.js";
+import activityRouter from "./routes/activityRoutes.js";
+import { initScheduler } from "./services/schedulerService.js";
 
 const app = express();
 //Databse Connection
@@ -25,6 +27,10 @@ app.use("/api/auth",authRouter)
 app.use("/api/oauth",socialAuthRouter)
 app.use("/api/accounts",accountRouter)
 app.use("/api/posts",postRouter)
+app.use("/api/activity",activityRouter)
+
+//Initialize Scheduler
+initScheduler()
 
 //Global Error Handler
 app.use((err:any,_req:Request,res:Response,_next:NextFunction)=>{
